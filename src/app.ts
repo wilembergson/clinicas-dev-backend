d46bed2ab8a554d5a39105f230f950606f9fee4d
wilembergson <wilembergson@yahoo.com>
2022-08-16 00:59:30 -0300

    Deploy 5

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { json } from "express"
 import "express-async-errors"
+import cors from "cors"
 
 import routerIndex from "./routers/routerIndex.js"
 import errorHandler from "./middlewares/errorHandlerMiddleware.js"
 
 const app = express()
 app.use(json())
+app.use(cors())
 
 app.use(routerIndex)
 app.use(errorHandler)
--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -4,5 +4,6 @@ export default function errorHandler (error, req: Request, res: Response, next: 
   if(error.error && error.status){
     return res.status(error.status).json({error: error.error})
   }
+  console.log(error)
   return res.status(500).json({error: 'Erro ao acessar o banco de dados.'})
 }
