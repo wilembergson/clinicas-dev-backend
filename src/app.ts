a086ee862d07cd0c2b2b84cd7d3aae33475cd64d
wilembergson <wilembergson@yahoo.com>
2022-08-16 00:31:39 -0300

    Deploy 4

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
 import express, { json } from "express"
-import cors from "cors"
 import "express-async-errors"
-import dotenv from "dotenv"
 
 import routerIndex from "./routers/routerIndex.js"
 import errorHandler from "./middlewares/errorHandlerMiddleware.js"
 
-dotenv.config()
-
 const app = express()
 app.use(json())
-app.use(cors())
 
 app.use(routerIndex)
 app.use(errorHandler)
